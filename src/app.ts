4c195ed5373402a8e19b17739d30da93ba9a051e
Brandon Barnes <imbrandonbarnes@gmail.com>
2024-09-18 18:58:43 -0700

    feat(github-actions-auto-deploy): Updated CORS to my local machine for testing and PROD

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173 || https://mana-vault.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }
 
