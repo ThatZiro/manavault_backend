5d51a7db5a2a69122190ace8a980c2ce856f6c6e
Brandon Barnes <imbrandonbarnes@gmail.com>
2024-09-18 19:38:20 -0700

    feat(github-HTTPS): Updated HTTPS Cert

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import app from './app';
 import sequelize from "./infrastructure/database/connect";
+import * as fs from "fs";
+import * as https from "https";
 
 const PORT = process.env.PORT || 3000;
 
+// Read SSL certificate files (self-signed or Let's Encrypt)
+const httpsOptions = {
+  key: fs.readFileSync('../selfsigned.key'),
+  cert: fs.readFileSync('../selfsigned.crt')
+};
+
 sequelize.sync({ force: false }).then(() => {
   console.log('Database synced');
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+
+  // Create HTTPS server
+  https.createServer(httpsOptions, app).listen(PORT, () => {
+    console.log(`HTTPS server is running on port ${PORT}`);
   });
 });
