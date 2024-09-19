7a043749a201242d3481020dad87c5b281d591f8
Brandon Barnes <imbrandonbarnes@gmail.com>
2024-09-19 16:51:53 -0700

    feat(github-HTTPS): Updated HTTPS Cert

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,18 @@ import app from './app';
 import sequelize from "./infrastructure/database/connect";
 import * as fs from "fs";
 import * as https from "https";
-import * as path from "path";
 
-// Read SSL certificate files (self-signed or Let's Encrypt)
+// Read Let's Encrypt SSL certificate files
 const httpsOptions = {
-  key: fs.readFileSync(path.join(__dirname, '../selfsigned.key')),
-  cert: fs.readFileSync(path.join(__dirname, '../selfsigned.crt'))
+  key: fs.readFileSync('/etc/letsencrypt/live/api.mana-vault.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/api.mana-vault.com/fullchain.pem')
 };
 
 sequelize.sync({ force: false }).then(() => {
   console.log('Database synced');
 
   // Create HTTPS server
-  https.createServer(httpsOptions, app).listen(8080, () => {
-    console.log(`HTTPS server is running on port 8080`);
+  https.createServer(httpsOptions, app).listen(8080, '0.0.0.0', () => {
+    console.log('HTTPS server is running on port 8080');
   });
 });
